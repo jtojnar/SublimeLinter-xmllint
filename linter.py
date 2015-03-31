959e77b9bc8949419625b7950c14a1384c0db03a
Jan Tojnar <jtojnar@gmail.com>
2015-03-31 14:50:41 +0200

    incorporated xmllintschema

--- linter.py
+++ linter.py
@@ -10,7 +10,9 @@
 
 """This module exports the Xmllint plugin class."""
 
-from SublimeLinter.lint import Linter, util
+from SublimeLinter.lint import Linter, util, persist
+from xml.dom import minidom
+import os
 
 
 class Xmllint(Linter):
@@ -18,13 +20,58 @@ class Xmllint(Linter):
     """Provides an interface to xmllint."""
 
     syntax = 'xml'
-    cmd = 'xmllint --noout * -'
+    executable = 'xmllint'
     regex = (
         r'^.+?:'
         r'(?P<line>\d+):.+?: '
         r'(?P<message>[^\r\n]+)\r?\n'
-        r'[^\r\n]*\r?\n'
-        r'(?P<col>[^\^]*)\^'
+        r'(?:[^\r\n]*\r?\n)?'
+        r'(?:(?P<col>[^\^]*)\^)?'
     )
     multiline = True
     error_stream = util.STREAM_STDERR
+
+    def run(self, cmd, code):
+        if not self.schema_exists:
+            # TODO: Replace 1 with correct line
+            return '-:1: schema error : Schema file does not exist\n'
+
+        return super().run(cmd, code)
+
+    def cmd(self):
+        """Return a list with the command line to execute."""
+
+        result = [self.executable_path, '--noout', '*']
+
+        if self.filename:
+            schema = self.schema_path()
+
+            if schema:
+                result.append('--schema')
+                result.append(schema)
+
+        result.append('-')
+
+        return result
+
+    def schema_path(self):
+        self.schema_exists = True
+        try:
+            xsi = 'http://www.w3.org/2001/XMLSchema-instance'
+            root = minidom.parseString(self.code).documentElement
+            schema = root.getAttributeNS(xsi, 'noNamespaceSchemaLocation')
+
+            if schema:
+                online = schema.find('http://') == 0 or schema.find('https://') == 0
+                if not (online or os.path.isabs(schema)):
+                    schema = os.path.dirname(self.filename) + '/' + schema
+
+                if not online and not os.path.isfile(schema):
+                    self.schema_exists = False
+                    return None
+
+                return schema
+        except Exception as e:
+            persist.debug(e)
+
+        return None
